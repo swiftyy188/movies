f3a39be558889603910a0a3dbcb1f64a92b952b1
swiftyy188 <jeffho.ml@gmail.com>
2018-01-05 15:35:43 -0500

    server created

---
+++ app.js
@@ -0,0 +1,32 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var path = require('path');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended': 'false'}));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+//catch 404 and forward to error handler
+app.use(function(req, res, next){
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next){
+	//set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'developement' ? err : {};
+
+	//render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+module.exports = app;
