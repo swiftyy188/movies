23d74bdd05723b7c89253d29f5dbf3c7fe9e21bc
swiftyy188 <jeffho.ml@gmail.com>
2018-01-10 15:55:23 -0500

    routes, html&css, bootstrap link added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CallbackComponent } from './callback.component';
 import { MoviesComponent } from './movies/movies.component';
 
+const appRoutes: Routes = [
+	{
+		path: 'movies',
+		component: MoviesComponent
+	},
+	{
+		path: '',
+		redirectTo: 'movies',
+		pathMatch: 'full'
+	}
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +25,12 @@ import { MoviesComponent } from './movies/movies.component';
     MoviesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+    	appRoutes,
+    	{ enableTracing: true}// debugging purpose only
+    	)
   ],
   providers: [],
   bootstrap: [AppComponent]
